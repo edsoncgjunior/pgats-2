486c48a0a645ce2920af5970a788a46d28b14b1c
Edson Júnior <edsonjunior@email.com>
2025-05-31 22:10:58 -0300

    feat: adiciona trabalho.js

---
+++ trabalho.js
@@ -0,0 +1,28 @@
+export function geradorDeTagsDeIdentificacao(nome) {
+  return nome.toUpperCase();
+}
+
+export function verificarSePodeSerAdotado(idade, porte) {
+  return idade === 1 && porte === 'M';
+}
+
+export function calcularConsumoDeRacao(nome, idade, peso) {
+  return peso * 300;
+}
+
+export function decidirTipoDeAtividadePorPorte(porte) {
+  const atividades = {
+    pequeno: 'brincar dentro de casa',
+    medio: 'caminhadas diárias',
+    grande: 'exercícios ao ar livre'
+  };
+  return atividades[porte];
+}
+
+export async function buscarDadoAsync() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Pipoca');
+    }, 10);
+  });
+}
